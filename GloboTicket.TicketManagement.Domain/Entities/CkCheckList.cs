7f10576a01905ca50ad45a056fa484355a0392e6
DaniyalBabar22 <147255848+DaniyalBabar22@users.noreply.github.com>
2024-03-28 14:19:05 +0500

    commit all
    commit all

--- GloboTicket.TicketManagement.Domain/Entities/CkCheckList.cs
+++ GloboTicket.TicketManagement.Domain/Entities/CkCheckList.cs
@@ -12,7 +12,6 @@ namespace ERPCubes.Domain.Entities
     {
         [Key]
         public int CLId { get; set; }
-        public int CPId { get; set; }
         public int TenantId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
